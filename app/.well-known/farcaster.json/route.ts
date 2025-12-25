a3d0a39c3a5df0120c9d3773959662c1f066370f
EAbdelilah <87987233+EAbdelilah@users.noreply.github.com>
2025-12-25 11:17:56 +0100

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -2,29 +2,29 @@ import { NextRequest, NextResponse } from 'next/server';
 
    export async function GET(req: NextRequest) {
      const manifest = {
-  "accountAssociation": {
-    "header": "eyJmaWQiOjE4NDk4MzIsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhGNzRkOTIyNDZCMDFkQTRDMWFEYTEzRTU0MEE4RTFhNzk2ODRBNWFlIn0",
-    "payload": "eyJkb21haW4iOiJld2FwLW1hcmdpbi52ZXJjZWwuYXBwIn0",
-    "signature": "VXMxJOwWeZzhrcEB+Tsld/QP4odivsK+b1lP0Z5K7V5jiliJxOrFhIBDbPITM8wWrikdwluNzSnUKo0NMVI1Xxw="
-  },
+       "accountAssociation": {
+        "header": "eyJmaWQiOjE4NDk4MzIsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhGNzRkOTIyNDZCMDFkQTRDMWFEYTEzRTU0MEE4RTFhNzk2ODRBNWFlIn0",
+        "payload": "eyJkb21haW4iOiJld2FwLW1hcmdpbi52ZXJjZWwuYXBwIn0",
+        "signature": "VXMxJOwWeZzhrcEB+Tsld/QP4odivsK+b1lP0Z5K7V5jiliJxOrFhIBDbPITM8wWrikdwluNzSnUKo0NMVI1Xxw="
+       },
        "miniapp": {
          "version": "1",
          "name": "Eswap Margin",
          "homeUrl": "https://ewap-margin.vercel.app/",
          "iconUrl": "https://ewap-margin.vercel.app/favicon.ico",
-         "splashImageUrl": "https://ewap-margin.vercel.app/splash.png",
+         "splashImageUrl": "https://ewap-margin.vercel.app/next.svg",
          "splashBackgroundColor": "#000000",
          "webhookUrl": "",
-         "subtitle": "Eswap DEX , Ending the funding fees",
-         "description": "The First 0% Interest Spot Margin DEX, Built on Top of Deep Liquidity.",
+         "subtitle": "A Decentralized Trading Platform",
+         "description": "The first 0, Built on Top of Deep Liquidity.",
          "screenshotUrls": [],
          "primaryCategory": "finance",
          "tags": ["trading", "dex", "base"],
-         "heroImageUrl": "https://ewap-margin.vercel.app/hero.png",
+         "heroImageUrl": "https://ewap-margin.vercel.app/next.svg",
          "tagline": "Trade with leverage on a decentralized platform.",
          "ogTitle": "Eswap Margin",
          "ogDescription": "The Next Generation of Decentralized Trading.",
-         "ogImageUrl": "https://ewap-margin.vercel.app/og.png",
+         "ogImageUrl": "https://ewap-margin.vercel.app/next.svg",
          "noindex": true
        }
      };
