b26976ac877f708ee536627fd130a6b2d9ada1e0
EAbdelilah <87987233+EAbdelilah@users.noreply.github.com>
2025-12-25 10:44:50 +0100

    Update route.ts
    account association

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -2,11 +2,12 @@ import { NextRequest, NextResponse } from 'next/server';
 
    export async function GET(req: NextRequest) {
      const manifest = {
-       "accountAssociation": {
-         "header": "",
-         "payload": "",
-         "signature": ""
-       },
+  "accountAssociation": {
+    "header": "eyJmaWQiOjE4NDk4MzIsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhGNzRkOTIyNDZCMDFkQTRDMWFEYTEzRTU0MEE4RTFhNzk2ODRBNWFlIn0",
+    "payload": "eyJkb21haW4iOiJld2FwLW1hcmdpbi52ZXJjZWwuYXBwIn0",
+    "signature": "VXMxJOwWeZzhrcEB+Tsld/QP4odivsK+b1lP0Z5K7V5jiliJxOrFhIBDbPITM8wWrikdwluNzSnUKo0NMVI1Xxw="
+  }
+},
        "miniapp": {
          "version": "1",
          "name": "Eswap Margin",
@@ -15,8 +16,8 @@ import { NextRequest, NextResponse } from 'next/server';
          "splashImageUrl": "https://ewap-margin.vercel.app/splash.png",
          "splashBackgroundColor": "#000000",
          "webhookUrl": "",
-         "subtitle": "A Decentralized Trading Platform",
-         "description": "The Next Generation of Decentralized Trading, Built on Top of Deep Liquidity.",
+         "subtitle": "Eswap DEX , Ending the funding fees",
+         "description": "The First 0% Interest Spot Margin DEX, Built on Top of Deep Liquidity.",
          "screenshotUrls": [],
          "primaryCategory": "finance",
          "tags": ["trading", "dex", "base"],
